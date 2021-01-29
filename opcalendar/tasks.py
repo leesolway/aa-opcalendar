77b7c104c84cfb25c94440ce631cc25dd26aced9
root <mail.seadc@gmail.com>
2021-01-29 09:39:58 +0200

    debug

--- opcalendar/tasks.py
+++ opcalendar/tasks.py
@@ -418,6 +418,157 @@ def import_fleets_without_fun_inc():
 		else:
 			logger.debug("Removed unseen NPSI fleets")
 			# Remove all events we did not see from API					
+			# Event.objects.filter(pk__in=event_ids_to_remove).delete()			
+
+# Import NPSI fleets
+@shared_task
+def import_fleets_without_fun_inc():
+	
+	#Get all current imported fleets in database
+	event_ids_to_remove = list(
+    	Event.objects.filter(visibility="import").values_list("id", flat=True)
+    )
+	local_tz = pytz.timezone("UTC")
+
+	#Get all import feeds
+	feeds = EventImport.objects.all()
+	
+	feed_errors = False
+
+	# Check for active NPSI feeds
+	for feed in feeds:
+		
+		# If Spectre Fleet is active
+		if feed.source=="Spectre Fleet":
+			
+			logger.debug("Spectre: import feed active. Pulling events from %s" % OPCALENDAR_SPECTRE_URL)
+			
+			try:
+				
+				# Get spectre fleets from ther RSS feed
+				d = feedparser.parse(OPCALENDAR_SPECTRE_URL)
+				
+				# Process each fleet entry
+				for entry in d.entries:
+					
+					##Look for SF fleets only
+					if entry.author_detail.name=='Spectre Fleet':
+						
+						#Only active fleets
+						if not "[RESERVED]" in entry.title:
+
+							logger.debug("Spectre: Import even found: %s" % entry.title)
+							
+							# Format datetimes
+							date_object = datetime.strptime(entry.published,'%a, %d %b %Y %H:%M:%S %z')
+							date_object.strftime('%Y-%m-%dT%H:%M')
+
+							# Check if we already have the event stored
+							original = Event.objects.filter(start_time=date_object, title=entry.title).first()
+
+							logger.debug("Spectre: Got match from database: %s" % original)
+
+							#If we get the event from API it should not be removed
+							if original is not None:
+								
+								logger.debug("Spectre: Event: %s already in database" % entry.title)
+								
+								# Remove the found fleet from the to be removed list
+								event_ids_to_remove.remove(original.id)
+
+							else:
+								# Save new fleet to database
+								event = Event(
+									operation_type=feed.operation_type,
+									title=entry.title,
+									host=feed.host,
+									doctrine="see details",
+									formup_system=feed.source,
+									description=entry.description,
+									start_time=date_object, 
+									end_time=date_object, 
+									fc=feed.source,
+									visibility="import",
+									user_id = feed.creator.id,
+									eve_character_id = feed.eve_character.id
+								)
+								
+								logger.debug("Spectre: Saved new event in database: %s" % entry.title)
+								
+								event.save()
+			except Exception as ex:
+				logger.error("Spectre: Error in fetching fleets", exc_info=True)
+				feed_errors = True
+				raise ex
+
+		# Check for EVE Uni events
+		if feed.source=="EVE University":
+			logger.debug("EVE Uni: import feed active. Pulling events from %s" % OPCALENDAR_EVE_UNI_URL)
+			
+			try:
+				#Get EVE Uni events from their API feed (ical)
+				url = OPCALENDAR_EVE_UNI_URL
+				c = Calendar(requests.get(url).text)
+				for entry in c.events:
+					
+					#Filter only class events as they are the only public events in eveuni
+					if "class" in entry.name.lower():
+
+						# Format datetime
+						start_date = datetime.utcfromtimestamp(entry.begin.timestamp).replace(tzinfo=pytz.utc)
+						end_date = datetime.utcfromtimestamp(entry.end.timestamp).replace(tzinfo=pytz.utc)
+						title = re.sub("[\(\[].*?[\)\]]", "", entry.name)
+
+						logger.debug("EVE Uni: Import even found: %s" % title)
+
+						# Check if we already have the event stored
+						original = Event.objects.filter(start_time=start_date, title=title).first()
+
+						logger.debug("EVE Uni: Got match from database: %s" % original)
+
+						#If we get the event from API it should not be removed	
+						if original is not None:
+							
+							logger.debug("EVE Uni: Event: %s already in database" % title)
+							
+							# Remove the found fleet from the to be removed list
+							event_ids_to_remove.remove(original.id)
+
+						else:		
+							# Save new event to database
+							event = Event(
+								operation_type=feed.operation_type,
+								title=title,
+								host=feed.host,
+								doctrine="see details",
+								formup_system=feed.source,
+								description=entry.description,
+								start_time=start_date, 
+								end_time=end_date, 
+								fc=feed.source,
+								visibility="import",
+								user_id = feed.creator.id,
+								eve_character_id = feed.eve_character.id
+							)
+
+							logger.debug("EVE Uni: Saved new EVE UNI event in database: %s" % title)
+							event.save()
+			
+			except Exception as ex:
+				logger.error("Spectre: Error in fetching fleets", exc_info=True)
+				feed_errors = True
+				raise ex
+
+	logger.debug("Checking for NPSI fleets to be removed.")
+	
+	if feed_errors:
+		logger.error("Errors in feeds, not cleaning up operations on this run")
+	else:
+		if not event_ids_to_remove:
+			logger.debug("No NPSI fleets to be removed.")
+		else:
+			logger.debug("Removed unseen NPSI fleets")
+			# Remove all events we did not see from API					
 			# Event.objects.filter(pk__in=event_ids_to_remove).delete()
 
 @shared_task(
