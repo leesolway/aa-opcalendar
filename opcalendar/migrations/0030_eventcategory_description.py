21eb77e5e67a1349e5895c222ad9483bc3be67d8
Ikarus Cesaille <mail.seadc@gmail.com>
2021-04-15 13:48:34 +0300

    precommit

--- opcalendar/migrations/0030_eventcategory_description.py
+++ opcalendar/migrations/0030_eventcategory_description.py
@@ -6,13 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('opcalendar', '0029_auto_20210410_0852'),
+        ("opcalendar", "0029_auto_20210410_0852"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='eventcategory',
-            name='description',
-            field=models.TextField(blank=True, help_text='Prefilled description that will be added on default on the event description.'),
+            model_name="eventcategory",
+            name="description",
+            field=models.TextField(
+                blank=True,
+                help_text="Prefilled description that will be added on default on the event description.",
+            ),
         ),
     ]
