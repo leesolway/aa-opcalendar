24b8aa846ab195470727927366b05f897022c1d3
Ikarus Cesaille <mail.seadc@gmail.com>
2021-05-02 19:30:16 +0300

    moon mining display formating

--- opcalendar/calendar.py
+++ opcalendar/calendar.py
@@ -97,11 +97,18 @@ class Calendar(HTMLCalendar):
                     if self.user.has_perm(
                         "moonmining.extractions_access"
                     ) and self.user.has_perm("moonmining.extractions_access"):
+                        refinery = event.refinery.name
+                        system = (
+                            event.refinery.moon.eve_moon.eve_planet.eve_solar_system.name
+                        )
+
+                        structure = refinery.replace(system, "")
+
                         d += (
                             f'<a class="nostyling" href="/moonmining/extraction/{event.id}?new_page=yes">'
                             f'<div class="event {"past-event" if datetime.now(timezone.utc) > event.chunk_arrival_at else "future-event"} event-moonmining">'
-                            f'<span>{event.chunk_arrival_at.strftime("%H:%M")} <i>Moon fracture at {event.refinery.moon}</i></span>'
-                            f"<span>{event.refinery}</span>"
+                            f'<span>{event.chunk_arrival_at.strftime("%H:%M")} <i>Moon fracture {event.refinery.moon.eve_moon.name}</i></span>'
+                            f"<span>{structure[3:]}</span>"
                             f"</div>"
                             f"</a>"
                         )
