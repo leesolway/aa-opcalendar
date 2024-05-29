2e887b34539522a25f4307acaa4a6cf385693747
root <root@AUTHDEV.myguest.virtualbox.org>
2024-05-29 17:47:49 +0300

    aa max version

--- opcalendar/__init__.py
+++ opcalendar/__init__.py
@@ -1,4 +1,4 @@
 default_app_config = "opcalendar.apps.OpcalendarConfig"
 
-__version__ = "3.0.0b1"
+__version__ = "3.0.0b2"
 __title__ = "Opcalendar"
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     install_requires=[
-        "allianceauth>=3.0.0",
+        "allianceauth<5.0.0,>=4",
         "feedparser",
         "ics>=0.7.2",
         "pytz",
